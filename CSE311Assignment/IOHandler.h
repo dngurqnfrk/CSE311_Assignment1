a2ea21d235b8f47942de4062b6d9481e4f1bb3ce
Jang <37791088+dngurqnfrk@users.noreply.github.com>
2025-03-30 23:50:58 +0900

    basic edit

---
+++ CSE311Assignment/IOHandler.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <fstream>
+#include <string>
+#include <vector>
+
+class IOHandler {
+private:
+public:
+	void get_input_txt(std::string addr);
+
+	IOHandler() = default;
+};
